13d516800d4e976b84eb8d4d988bd79c0ca26ce4
MSandeman <msandermann96@gmail.com>
2018-12-07 12:51:37 +0100

    Add Filtermatrix

--- src/main/java/com/bv_gruppe_d/imagej/Filtermatrix.java
+++ src/main/java/com/bv_gruppe_d/imagej/Filtermatrix.java
@@ -2,4 +2,15 @@ package com.bv_gruppe_d.imagej;
 
 public abstract class Filtermatrix {
 
+    int matrix[];
+    double factor;
+    int hotSpot;
+
+    public Filtermatrix() {
+
+    }
+
+    public abstract void createMatrix();
+
+    public abstract void calculateMatrix();
 }
